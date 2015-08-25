bb2cea73d4232bdf0e3d470d886bad7a74c069b1
Allyson Alves de Souza <allyson@hastedesign.com.br>
2015-08-25 20:43:52 -0300

    Removido Bookings do repositório (teste de compatibilidade)

--- wp-content/plugins/woocommerce-bookings/includes/admin/views/html-booking-tab.php
+++
@@ -1,4 +0,0 @@
-<li class="bookings_tab bookings_resources_tab advanced_options show_if_booking"><a href="#bookings_resources"><?php _e( 'Resources', 'woocommerce-bookings' ); ?></a></li>
-<li class="bookings_tab bookings_availability_tab advanced_options show_if_booking"><a href="#bookings_availability"><?php _e( 'Availability', 'woocommerce-bookings' ); ?></a></li>
-<li class="bookings_tab bookings_pricing_tab advanced_options show_if_booking"><a href="#bookings_pricing"><?php _e( 'Costs', 'woocommerce-bookings' ); ?></a></li>
-<li class="bookings_tab bookings_persons_tab advanced_options show_if_booking"><a href="#bookings_persons"><?php _e( 'Persons', 'woocommerce-bookings' ); ?></a></li>
--- wp-content/plugins/woocommerce-bookings/templates/booking-form/select.php
+++
@@ -1,9 +0,0 @@
-<?php extract( $field ); ?>
-<p class="form-field form-field-wide <?php echo implode( ' ', $class ); ?>">
-	<label for="<?php echo $name; ?>"><?php echo $label; ?>:</label>
-	<select name="<?php echo $name; ?>" id="<?php echo $name; ?>">
-		<?php foreach ( $options as $key => $value ) : ?>
-			<option value="<?php echo $key; ?>"><?php echo $value; ?></option>
-		<?php endforeach; ?>
-	</select>
-</p>
