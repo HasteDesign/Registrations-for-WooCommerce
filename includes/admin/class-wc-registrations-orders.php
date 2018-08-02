0cbe14ae492e0376cd81fe22dc4522675cc63def
ajlowndes <ajlowndes@gmail.com>
2018-08-02 13:33:21 +1000

    add registrations column to orders page

--- includes/admin/class-wc-registrations-orders.php
+++ includes/admin/class-wc-registrations-orders.php
@@ -23,7 +23,7 @@ class WC_Registrations_Orders {
     // Add new shop order column
     add_filter( 'manage_edit-shop_order_columns', __CLASS__ . '::add_registration_details_column' );
     // And fill the shop order column with the registrations data
-    add_action( 'manage_shop_order_posts_custom_column', __CLASS__ . '::add_course_details_column_content' );
+    add_action( 'manage_shop_order_posts_custom_column', __CLASS__ . '::add_registration_details_column_content' );
   }
 
   /**
@@ -34,8 +34,7 @@ class WC_Registrations_Orders {
   public static function add_registration_details_column( $columns ) {
       $new_columns = ( is_array( $columns ) ) ? $columns : array();
       unset( $new_columns[ 'billing_address' ] );
-      $new_columns['course_booked'] = 'Course(s) booked';
-      // $new_columns['course_booked'] = __( 'Course(s) booked', 'woocommerce' );
+      $new_columns['registration_booked'] = 'Registration(s)';
       $new_columns[ 'billing_address' ] = $columns[ 'billing_address' ];
       return $new_columns;
   }
@@ -45,12 +44,12 @@ class WC_Registrations_Orders {
    *
    * @since 1.0.7
    */
-  public static function add_course_details_column_content ( $column ) {
+  public static function add_registration_details_column_content ( $column ) {
     global $post, $the_order;
     if ( empty( $the_order ) || $the_order->get_id() != $post->ID ) {
           $the_order = wc_get_order( $post->ID );
         }
-    if ( $column === 'course_booked' ){
+    if ( $column === 'registration_booked' ){
       $items  = $the_order->get_items();
       $patterns = array ('/(19|20)(\d{2})-(\d{1,2})-(\d{1,2})/',
                      '/\{"type":.*":/',
