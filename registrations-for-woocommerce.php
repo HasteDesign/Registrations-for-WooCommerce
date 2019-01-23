15b0844909d378c6b73bf9fa3e29aa5f25dbc280
Allyson <allyson@hastedesign.com.br>
2019-01-23 18:46:12 -0200

    Implementes a class to manage date import trough WC improter

--- registrations-for-woocommerce.php
+++ registrations-for-woocommerce.php
@@ -137,6 +137,7 @@ class WC_Registrations {
 		require_once( 'includes/class-wc-registrations-admin.php' );
 		require_once( 'includes/class-wc-registrations-cart.php' );
 		require_once( 'includes/class-wc-registrations-helpers.php' );
+		require_once( 'includes/class-wc-registrations-import.php' );
 		require_once( 'includes/admin/class-wc-registrations-orders.php' );
 		require_once( 'includes/reports/class-wc-reports-manager.php' );
 	}
